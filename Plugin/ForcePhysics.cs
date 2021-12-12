0bc55b45c18146c7a44fb17000f78eb1e2759566
fkszkksz <fkszkksz@asd.com>
2021-12-12 05:53:16 -0800

    Initial commit (working for U10)

---
+++ Plugin/ForcePhysics.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+
+using UnityEngine;
+using ThunderRoad;
+
+
+namespace ForcePhysics
+{
+    public class ForcePhysicsModule : MonoBehaviour
+    {
+
+        public bool force_enabled = true;
+
+        Creature creature;
+        
+        private void Awake()
+        {
+            Ragdoll ragdoll = gameObject.GetComponentInChildren<Ragdoll>();
+            this.creature = ragdoll.creature;
+            this.creature.SetFaction(UnityEngine.Random.Range(0, 5));
+        }
+
+        private void LateUpdate()
+        {
+            if (force_enabled && !this.creature.ragdoll.creature.isKilled)
+            {
+
+                if (!this.creature.hidden)
+                {
+                    Logger.Detailed("Forcing physics for {0} ({1}, {2})", creature.name, creature.creatureId, creature.GetInstanceID());
+                    this.creature.ragdoll.AddPhysicToggleModifier(this);
+                }
+                else
+                {
+                    Logger.Detailed("Not forcing physics for {0} ({1}, {2})", creature.name, creature.creatureId, creature.GetInstanceID());
+                    this.creature.ragdoll.RemovePhysicToggleModifier(this);
+                }
+            }
+            else
+            {
+                Logger.Detailed("Not forcing physics for {0} ({1}, {2})", creature.name, creature.creatureId, creature.GetInstanceID());
+                this.creature.ragdoll.RemovePhysicToggleModifier(this);
+            }
+        }
+    }
+}
---
+++ Plugin/LoadModule.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ThunderRoad;
+using UnityEngine;
+
+namespace ForcePhysics
+{
+    public class LoadModule : LevelModule
+    {
+
+        public string mod_version = "0.0";
+        public string mod_name = "UnnamedMod";
+        public string logger_level = "Basic";
+
+        private bool _enabled = true;
+        public bool enabled
+        {
+            get
+            {
+                return _enabled;
+            }
+            set
+            {
+                _enabled = value;
+                foreach (Creature creature in creatureList)
+                {
+                    if (creature.gameObject.TryGetComponent<ForcePhysicsModule>(out ForcePhysicsModule module))
+                    {
+                        module.force_enabled = _enabled;
+                    }
+                }
+            }
+        }
+
+        LinkedList<Creature> creatureList = new LinkedList<Creature>();
+
+        public override IEnumerator OnLoadCoroutine()
+        {
+            Logger.init(mod_name, mod_version, logger_level);
+
+            Logger.Basic("Loading " + mod_name);
+            EventManager.onCreatureSpawn += EventManager_onCreatureSpawn;
+            return base.OnLoadCoroutine();
+        }
+
+        private void EventManager_onCreatureSpawn(Creature creature)
+        {
+            if (!creature.isPlayer)
+            {
+                if (!creature.gameObject.TryGetComponent<ForcePhysicsModule>(out ForcePhysicsModule _))
+                {
+                    Logger.Detailed(String.Format("Adding ForcePhysicsModule to {0} ({1}, {2})", creature.name, creature.creatureId, creature.GetInstanceID()));
+                    var component = creature.gameObject.AddComponent<ForcePhysicsModule>();
+                    creatureList.AddLast(creature);
+                    creature.OnDespawnEvent += delegate {
+                        GameObject.Destroy(component);
+                        creatureList.Remove(creature);
+                    };
+                }
+            }
+        }
+    }
+}
